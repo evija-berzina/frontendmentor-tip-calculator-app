410b1b80848ce10e9793f5279cfe98094520664c
eberzina <berzina3633@gmail.com>
2026-01-25 17:56:19 +0200

    Add error messages

--- main.js
+++ main.js
@@ -5,41 +5,70 @@ const numberOfPeople = document.querySelector('.number-of-people-js');
 const tipResult = document.querySelector('.tip-amount-js');
 const totalResult = document.querySelector('.total-js');
 const resetBtn = document.querySelector('.reset-btn-js');
+const billErrorMsg = document.querySelector('.bill-error-msg-js');
+const tipErrorMsg = document.querySelector('.tip-error-msg-js');
+const peopleErrorMsg = document.querySelector('.people-error-msg-js');
 
 let bill = 0;
 let tip = 0;
 let peopleCount = 0;
 
 billInput.addEventListener('input', () => {
-  bill = Number(billInput.value) || 0;
+  bill = Number(billInput.value);
+
+  if (billInput.value === '') {
+    billErrorMsg.hidden = true;
+    bill = 0;
+  } else if (bill <= 0) {
+    billErrorMsg.textContent = 'Invalid value';
+    billErrorMsg.hidden = false;
+  } else {
+    billErrorMsg.hidden  = true;
+  }
+
   updateResults();
 });
 
 tipBtn.forEach(input => {
   input.addEventListener('click', () => {
     tip = Number(input.dataset.value);
+    tipInput.value = '';
     updateResults();
   })
 });
 
 tipInput.addEventListener('input', () => {
   const tipValue = Number(tipInput.value);
-  if (Number.isInteger(tipValue) && tipValue > 0) {
-    tip = tipValue;
-    updateResults();
+
+  if (tipInput.value === '') {
+    tipErrorMsg.hidden = true;
+    tip = 0;
+  } else if (!Number.isInteger(tipValue) || tipValue <= 0) {
+    tipErrorMsg.textContent = 'Invalid value';
+    tipErrorMsg.hidden = false;
   } else {
-    renderResults();
+    tip = tipValue;
+    tipErrorMsg.hidden = true;
   }
+
+  updateResults();
 });
 
 numberOfPeople.addEventListener('input', () => {
   const people = Number(numberOfPeople.value);
-  if (Number.isInteger(people) && people > 0) {
-    peopleCount = people;
-    updateResults();
+    
+  if (numberOfPeople.value === '') {
+    peopleErrorMsg.hidden = true;
+    peopleCount = 0;
+  } else if (!Number.isInteger(people) || people <= 0) {
+    peopleErrorMsg.textContent = 'Invalid value';
+    peopleErrorMsg.hidden = false;
   } else {
-    renderResults();
+    peopleCount = people;
+    peopleErrorMsg.hidden = true;
   }
+
+  updateResults();
 });
 
 resetBtn.addEventListener('click', () => {
@@ -65,13 +94,9 @@ function updateResults() {
     const total = totalCalculation(tipAmount);
     totalResult.textContent = total.toFixed(2);
   } else {
-    renderResults();
+    tipResult.textContent = '0.00';
+    totalResult.textContent = '0.00';
   }
-}
-
-function renderResults () {
-  tipResult.textContent = '0.00';
-  totalResult.textContent = '0.00';
 }
 
 function renderTipCalculator() {
@@ -81,7 +106,8 @@ function renderTipCalculator() {
   billInput.value = '';
   tipInput.value = '';
   numberOfPeople.value = '';
-  renderResults();
+  tipResult.textContent = '0.00';
+  totalResult.textContent = '0.00';
 }
 
 updateResults();
