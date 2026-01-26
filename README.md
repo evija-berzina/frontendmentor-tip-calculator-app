2674a485dd5b7e7e8a430fc200e241f3495f0a38
eberzina <berzina3633@gmail.com>
2026-01-26 15:31:48 +0200

    Add hover effects and hidden button

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Frontend Mentor - Tip calculator app
 
 ![Design preview for the Tip calculator app coding challenge](./preview.jpg)
+
+## 🔗 Live Demo
+
+[View Live Demo](https://evija-berzina.github.io/frontendmentor-tip-calculator-app/)
--- main.js
+++ main.js
@@ -18,20 +18,33 @@ billInput.addEventListener('input', () => {
 
   if (billInput.value === '') {
     billErrorMsg.hidden = true;
+    billInput.classList.remove('error-border-bill');
+    billInput.classList.add('bill-input');
     bill = 0;
   } else if (bill <= 0) {
     billErrorMsg.textContent = 'Invalid value';
     billErrorMsg.hidden = false;
+    billInput.classList.add('error-border-bill');
+    billInput.classList.remove('bill-input');
   } else {
     billErrorMsg.hidden  = true;
+    billInput.classList.remove('error-border-bill');
+    billInput.classList.add('bill-input');
   }
 
   updateResults();
 });
 
-tipBtn.forEach(input => {
-  input.addEventListener('click', () => {
-    tip = Number(input.dataset.value);
+tipBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    tip = Number(btn.dataset.value);
+    tipBtn.forEach(b => {
+      if(b === btn) {
+        b.setAttribute('aria-checked', true);
+      } else {
+        b.setAttribute('aria-checked', false);
+      }
+    })
     tipInput.value = '';
     updateResults();
   })
@@ -39,16 +52,23 @@ tipBtn.forEach(input => {
 
 tipInput.addEventListener('input', () => {
   const tipValue = Number(tipInput.value);
+  tipBtn.forEach(btn => btn.setAttribute('aria-checked', false));
 
   if (tipInput.value === '') {
     tipErrorMsg.hidden = true;
     tip = 0;
+    tipInput.classList.remove('error-border-tip');
+    tipInput.classList.add('tip-input');
   } else if (!Number.isInteger(tipValue) || tipValue <= 0) {
     tipErrorMsg.textContent = 'Invalid value';
     tipErrorMsg.hidden = false;
+    tipInput.classList.add('error-border-tip');
+    tipInput.classList.remove('tip-input');
   } else {
     tip = tipValue;
     tipErrorMsg.hidden = true;
+    tipInput.classList.remove('error-border-tip');
+    tipInput.classList.add('tip-input');
   }
 
   updateResults();
@@ -60,12 +80,18 @@ numberOfPeople.addEventListener('input', () => {
   if (numberOfPeople.value === '') {
     peopleErrorMsg.hidden = true;
     peopleCount = 0;
+    numberOfPeople.classList.remove('error-border-people');
+    numberOfPeople.classList.add('number-of-people-input');
   } else if (!Number.isInteger(people) || people <= 0) {
     peopleErrorMsg.textContent = 'Invalid value';
     peopleErrorMsg.hidden = false;
+    numberOfPeople.classList.add('error-border-people');
+    numberOfPeople.classList.remove('number-of-people-input');
   } else {
     peopleCount = people;
     peopleErrorMsg.hidden = true;
+    numberOfPeople.classList.remove('error-border-people');
+    numberOfPeople.classList.add('number-of-people-input');
   }
 
   updateResults();
@@ -89,6 +115,7 @@ function totalCalculation(tipAmount) {
 
 function updateResults() {
   if (bill > 0 && tip > 0 && peopleCount > 0) {
+    resetBtn.disabled = false;
     const tipAmount = tipAmountCalculation();
     tipResult.textContent = tipAmount.toFixed(2);
     const total = totalCalculation(tipAmount);
@@ -96,6 +123,7 @@ function updateResults() {
   } else {
     tipResult.textContent = '0.00';
     totalResult.textContent = '0.00';
+    resetBtn.disabled = true;
   }
 }
 
@@ -104,10 +132,12 @@ function renderTipCalculator() {
   tip = 0;
   peopleCount = 0;
   billInput.value = '';
+  tipBtn.forEach(btn => btn.setAttribute('aria-checked', false));
   tipInput.value = '';
   numberOfPeople.value = '';
   tipResult.textContent = '0.00';
   totalResult.textContent = '0.00';
+  resetBtn.disabled = true;
 }
 
 updateResults();
